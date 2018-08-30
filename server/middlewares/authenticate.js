2d44d34af305f5625cfb8d0cbc89e272091340c6
tes3awy <osaid2512@gmail.com>
2018-08-30 02:03:25 +0200

    Add flash message for unauthorised access

--- server/middlewares/authenticate.js
+++ server/middlewares/authenticate.js
@@ -2,6 +2,7 @@ var authenticate = (req, res, next) => {
   if(req.user) {
     return next();
   }
+  req.flash('error', { msg: 'Please login to view this page' });
   res.redirect('/auth/login');
 };
 
