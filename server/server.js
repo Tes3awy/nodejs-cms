c1e05ea13fb540a6dd01baba52725864b720ad29
tes3awy <osaid2512@gmail.com>
2018-09-19 00:01:25 +0200

    Convert indentation into 2 spaces

--- server/server.js
+++ server/server.js
@@ -43,9 +43,9 @@ const port = process.env.PORT || 3000;
 app.use(favicon(path.join(__dirname, publicPath, "favicon.ico")));
 // Serve Statics Middleware
 app.use(
-    serveStatic(path.join(__dirname, publicPath), {
-        dotfiles: "deny"
-    })
+  serveStatic(path.join(__dirname, publicPath), {
+    dotfiles: "deny"
+  })
 );
 
 /** Middlewares */
@@ -56,17 +56,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression({ level: 1 }));
 // Session Middleware
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        name: "sessionid",
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            secure: false,
-            maxAge: 3600000,
-            expires: new Date(Date.now() + 3600000)
-        }
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    name: "sessionid",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000)
+    }
+  })
 );
 // Passport Middlewares
 app.use(passport.initialize());
@@ -75,55 +75,55 @@ app.use(passport.session());
 app.use(flash());
 // Error Handler
 if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
-    app.use(errorhandler());
+  app.use(errorhandler());
 }
 // Global Vars
 app.use(function(req, res, next) {
-    res.locals.user = req.user || null;
-    next();
+  res.locals.user = req.user || null;
+  next();
 });
 // Morgan Middleware
 app.use(logger("dev"));
 // Exp-hbs View Engine Middleware
 app.engine(
-    "hbs",
-    hbs({
-        defaultLayout: "main",
-        extname: ".hbs",
-        partialsDir: "views/partials",
-        layoutsDir: "views/layouts",
-        // express-handlebars helper functions
-        helpers: {
-            getPrettyDate(date) {
-                return moment(date).format("lll");
-            },
-            prettyCreatedAt(date) {
-                return moment(date).format("ll");
-            },
-            getToday() {
-                return moment().format("MMMM Do YYYY");
-            },
-            getCurrentYear() {
-                return new Date().getFullYear();
-            },
-            truncateText(content) {
-                return _.truncate(content, { length: 300 });
-            },
-            descriptionTruncate(description) {
-                return _.truncate(description, { length: 220 });
-            },
-            capitalize(tag) {
-                return _.capitalize(tag);
-            },
-            if_eq(a, b, opts) {
-                if (a === b) {
-                    return opts.fn(this);
-                } else {
-                    return opts.inverse(this);
-                }
-            }
+  "hbs",
+  hbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    partialsDir: "views/partials",
+    layoutsDir: "views/layouts",
+    // express-handlebars helper functions
+    helpers: {
+      getPrettyDate(date) {
+        return moment(date).format("lll");
+      },
+      prettyCreatedAt(date) {
+        return moment(date).format("ll");
+      },
+      getToday() {
+        return moment().format("MMMM Do YYYY");
+      },
+      getCurrentYear() {
+        return new Date().getFullYear();
+      },
+      truncateText(content) {
+        return _.truncate(content, { length: 300 });
+      },
+      descriptionTruncate(description) {
+        return _.truncate(description, { length: 220 });
+      },
+      capitalize(tag) {
+        return _.capitalize(tag);
+      },
+      if_eq(a, b, opts) {
+        if (a === b) {
+          return opts.fn(this);
+        } else {
+          return opts.inverse(this);
         }
-    })
+      }
+    }
+  })
 );
 app.set("view engine", "hbs");
 // Method Override Middleware
@@ -139,10 +139,10 @@ app.use("/tag", tagsRoutes);
 
 // Serving locally on port 3000
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server started on port ${port}`);
-    console.log("NODE_ENV:", `${process.env.NODE_ENV} environment`);
+  console.log(`Server started on port ${port}`);
+  console.log("NODE_ENV:", `${process.env.NODE_ENV} environment`);
 });
 
 module.exports = {
-    app
+  app
 };
